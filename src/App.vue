1cfb9d9b4b16f8c8ae291715ffc944a841a6ed5e
BuildTools <unconfigured@null.spigotmc.org>
2020-01-18 21:45:03 -0600

    set browsint

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- src/components/CardSpoiler.vue
+++ src/components/CardSpoiler.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>Prepare to have cards spoiled!</h1>
-    <p>
-      There is not much here yet, and it certainly doesn't do much, but you are loading a web page with locally stored card data.
-    </p>
-    <b-form-input v-model="filter" placeholder="Filter text" style="margin:5px 20%;width:60%;text-align:center"/>
-    <template v-if="cards">
-      <ul>
-        <li v-for="card in cardsFiltered" :key="card.name">{{ card.name }}</li>
-      </ul>
-    </template>
-    <template v-else>
-      <span>Loading...</span>
-    </template>
+  <div class="spoiler-page">
+    <div class="spoiler-header">
+      <h1>Prepare to have cards spoiled!</h1>
+      <p>
+        There is not much here yet, and it certainly doesn't do much, but you are loading a web page with locally stored card data.
+      </p>
+      <b-form-input v-model="filter" placeholder="Filter text" style="margin:5px 20%;width:60%;text-align:center"/>
+    </div>
+    <div class="spoiler-container">
+      <template v-if="cards">
+        <div class="set-sidebar">
+          <ul>
+            <li :style="{ fontWeight: selectedSet == null ? 'bold' : 'normal' }"
+                @click="selectedSet = null">All</li>
+            <li v-for="set in setList"
+                :key="set"
+                :style="{ fontWeight: set === selectedSet ? 'bold' : 'normal' }"
+                @click="selectedSet = set">
+              {{ set }}
+            </li>
+          </ul>
+        </div>
+        <div class="card-sidebar">
+          <ul>
+            <li v-for="card in cardsFiltered"
+                :key="card.name"
+                :style="{ fontWeight: card === selectedCard ? 'bold' : 'normal' }"
+                @click="selectedCard = card">
+              {{ card.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="card-region">
+          <pre>{{ JSON.stringify(selectedCard, null, 2) }}</pre>
+        </div>
+      </template>
+      <template v-else>
+        <span>Loading...</span>
+      </template>
+    </div>
   </div>
 </template>
 
@@ -24,13 +50,31 @@ export default {
   data() {
     return {
       cards: null,
-      filter: ""
+      filter: "",
+      selectedSet: null,
+      selectedCard: null
     }
   },
   computed: {
+    setList() {
+      let sets = {};
+      this.cards.forEach(x => {
+        x.setInfos.forEach(y => {
+          sets[y.set] = true;
+        });
+      });
+      return Object.keys(sets).sort()
+    },
     cardsFiltered() {
       let filter = this.filter.toLowerCase()
-      return this.cards.filter(x => x.name.toLowerCase().includes(filter))
+      return this.cards.filter(x => {
+        if (this.selectedSet && !x.setInfos.filter(y => y.set === this.selectedSet)[0]) return false;
+        return x.name.toLowerCase().includes(filter)
+      }).sort((a, b) => {
+        if (a.name < b.name) return -1
+        else if (a.name > b.name) return 1
+        else return 0
+      })
     }
   },
   mounted() {
@@ -48,18 +92,52 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.spoiler-page {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.spoiler-container {
+  display: flex;
+  flex: 1;
+  overflow-y: hidden;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.set-sidebar {
+  flex-basis: 100px;
+  margin-left: 5px;
+  overflow-y: scroll;
 }
-a {
-  color: #42b983;
+  .set-sidebar ul {
+    list-style-type: none;
+    padding: 0;
+    text-align: left;
+  }
+  .set-sidebar li {
+    cursor: pointer;
+  }
+
+.card-sidebar {
+  flex-basis: 250px;
+  margin-left: 5px;
+  overflow-y: scroll;
+}
+  .card-sidebar ul {
+    list-style-type: none;
+    padding: 0;
+    text-align: left;
+  }
+  .card-sidebar li {
+    cursor: pointer;
+  }
+
+.card-region {
+  flex: 1;
+  text-align: left;
+  font-family: 'Courier New', Courier, monospace;
+  overflow: auto;
+  height: 100%;
 }
 </style>
